87f7799574bd12357c7fea833eadd11b752e61cb
Stefan Schindler <dns2utf8@estada.ch>
2021-11-24 16:58:01 +0100

    Migrate to edition = 2018

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,16 +1,24 @@
 # CHANGELOG
 
-## 0.1.1 2017-
+## 0.3.0 2020-11-24
+
+* Update to edition 2018
+
+## 0.2.1 2017-09-23
+
 * Add Windows CI
 * Add badges
 
 ## 0.1.0 2017-09-01
+
 * Fix wording
 
 ## 0.0.2 2017-08-08
+
 * Fix typos
 
 ## 0.0.1 2017-07-13
+
 * Return the guarded object to the closure handling the drop
 
 ## 0.0.0 2017-07-13
--- examples/rainbow.rs
+++ examples/rainbow.rs
@@ -4,10 +4,12 @@ use drop_guard::DropGuard;
 
 fn main() {
     let s = String::from("a commonString");
-    let mut s = DropGuard::new(s, |final_string| println!("s became {} at last", final_string));
-    
+    let mut s = DropGuard::new(s, |final_string| {
+        println!("s became {} at last", final_string)
+    });
+
     // much code and time passes by ...
     *s = "a rainbow".to_string();
-    
+
     // by the end of this function the String will have become a rainbow
 }
